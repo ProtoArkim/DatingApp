466eb928171c492b48a6b544d88f72b0553b673c
Alvarez <luisantonio.alvarez@fiserv.com>
2022-09-07 13:38:16 -0600

    End of section 14

--- API/Controllers/Buggycontroller.cs
+++ API/Controllers/Buggycontroller.cs
@@ -24,7 +24,7 @@ namespace API.Controllers
             return "secret text";
         }
 
-         [HttpGet("not-found")]
+        [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
             var thing = _context.Users.Find(-1);
@@ -32,7 +32,7 @@ namespace API.Controllers
             return Ok(thing);
         }
 
-         [HttpGet("server-error")]
+        [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
             var thing = _context.Users.Find(-1);
@@ -41,10 +41,10 @@ namespace API.Controllers
             
         }
 
-         [HttpGet("bad-request")]
+        [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
-            return BadRequest("This was not a good request");
+            return BadRequest();
         }
     }
 }
--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -21,16 +21,18 @@ export class ErrorInterceptor implements HttpInterceptor {
         if(error){
           switch (error.status) {
             case 400:
-              if(error.error.errors){
-                const modalStateErrors = []
-                for(const key in error.error.errors){
-                  if(error.error.errors[key]){
+              if(error.error.errors) {
+                const modalStateErrors = [];
+                for(const key in error.error.errors) {
+                  if(error.error.errors[key]) {
                     modalStateErrors.push(error.error.errors[key])
                   }
                 }
-                throw modalStateErrors.flat()
+                throw modalStateErrors.flat();
+              } else if(typeof(error.error) == 'object'){
+                this.toast.error(error.statusText, error.status);
               }else{
-                this.toast.error(error.statusText, error.status)
+                this.toast.error(error.error, error.status)
               }
               break;
             case 401:
